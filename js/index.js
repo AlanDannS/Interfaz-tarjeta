0b31f733066b06b29a978be2524cad002ef840a4
Daniel <danielnun9719@gmail.com>
2020-05-11 01:00:47 -0500

    Se ha terminado toda la parte html y css del proyecto

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 const tarjeta = document.querySelector('#tarjeta')
 const btnAbrirFormulario = document.querySelector('#btn-abrir-formulario')
+const formulario = document.querySelector('#formulario-tarjeta')
 tarjeta.addEventListener('click', () => {
     tarjeta.classList.toggle('active')
 })
 
 btnAbrirFormulario.addEventListener('click', () => {
     btnAbrirFormulario.classList.toggle('active')
+    formulario.classList.toggle('active')
 })
